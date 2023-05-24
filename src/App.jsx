0868fff51a63a9bf7459d79509cd4d0d4b84ab41
Youkwhan <Youkwhan@gmail.com>
2023-05-24 18:06:11 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+	const [game, setGame] = useState(false)
+	return (
+		<main>
+			{game ? (
+				<div>Hello</div>
+			) : (
+				<div className="start-quiz">
+					<h1>Quizzical</h1>
+					<p className="start-quiz__description">Some description if needed</p>
+					<button className="start-quiz__btn">Start quiz</button>
+				</div>
+			)}
+		</main>
+	)
+}
+
+export default App
