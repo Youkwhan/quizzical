c96dd0f82df1bf417b7241f1a4dc26209fab552d
Youkwhan <Youkwhan@gmail.com>
2023-05-25 15:58:24 -0400

    blobs background

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ function App() {
 	const [game, setGame] = useState(false)
 	return (
 		<main>
-			{game ? (
-				<div>Hello</div>
-			) : (
-				<div className="start-quiz">
-					<h1>Quizzical</h1>
-					<p className="start-quiz__description">Some description if needed</p>
-					<button className="start-quiz__btn btn">Start quiz</button>
-				</div>
-			)}
+			<div className="blob blob-top"></div>
+			<div className="blob blob-bot"></div>
+
+			<div className="content-container">
+				{game ? (
+					<div>Hello</div>
+				) : (
+					<div className="start-quiz">
+						<h1>Quizzical</h1>
+						<p className="start-quiz__description">
+							Some description if needed
+						</p>
+						<button className="start-quiz__btn btn">Start quiz</button>
+					</div>
+				)}
+			</div>
 		</main>
 	)
 }
